b57add10be19bdcbb6435f3af3470aca9a4c8f5a
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-07-29 17:44:51 +0200

    usability adjustments to UVarInt
    - make comparable
    - add toString
    - specifically require either full-bytes decode or leading decode
    - add length return for leading decode

--- multibase/src/commonMain/kotlin/at/asitplus/io/UVarInt.kt
+++ multibase/src/commonMain/kotlin/at/asitplus/io/UVarInt.kt
@@ -8,15 +8,15 @@ package at.asitplus.io
 /**
  * Unsigned variable-length integer supporting values up to 2^63 - 1.
  */
-class UVarInt private constructor(private val number: ULong) {
+data class UVarInt private constructor(private val number: ULong) {
     /**
      * Convenience constructor to create an object from an unsigned int. To create larger UVarInts, use [fromByteArray].
      */
     constructor(number: UInt) : this(number.toULong())
 
-    /**
-     * Returns the ULong value of this UVarInt.
-     */
+    override fun toString() = "0x"+number.toString(16)
+
+    /** Returns the ULong value of this UVarInt. */
     fun toULong(): ULong = number
 
     /**
@@ -46,29 +46,50 @@ class UVarInt private constructor(private val number: ULong) {
          * @throws NumberFormatException on illegal input (e.g. values larger than 2^63 - 1 or non-minimal encodings)
          */
         @Throws(NumberFormatException::class)
-        fun fromByteArray(bytes: ByteArray): UVarInt = UVarInt(decode(bytes))
+        @Deprecated(message="Specify explicitly whether you want to consume the entire byte array" +
+                "(use fromByteArrayStrict or fromByteArrayPermissive)")
+        fun fromByteArray(bytes: ByteArray): UVarInt = fromByteArrayPermissive(bytes).first
 
-        private fun decode(encoded: ByteArray): ULong {
+        /**
+         * Decodes the entirety of [bytes] into an [UVarInt].
+         * @throws NumberFormatException on illegal input (e.g. values larger than 2^63 - 1 or non-minimal encodings)
+         * @throws IllegalArgumentException if the entire ByteArray was not consumed
+         */
+        fun fromByteArrayStrict(bytes: ByteArray): UVarInt =
+            decode(bytes).let {
+                require(it.second == bytes.size) {
+                    "UVarInt decoding only consumed ${it.second} bytes, but ${bytes.size} were provided."
+                }
+                UVarInt(it.first)
+            }
+
+        /**
+         * Decodes the leading UVarInt from [bytes].
+         * Returns the UVarInt and the number of leading bytes consumed.
+         * @throws NumberFormatException on illegal input (e.g. values larger than 2^63 - 1 or non-minimal encodings)
+         */
+        fun fromByteArrayPermissive(bytes: ByteArray): Pair<UVarInt,Int> =
+            decode(bytes).let { Pair(UVarInt(it.first),it.second) }
+
+        /** Decodes a varint-encoded leading ByteArray */
+        private fun decode(encoded: ByteArray): Pair<ULong,Int> {
             var value = 0uL
-            var s = 0
             var i = 0
             while (true) {
-                val uByte = encoded[i].toUByte()
-                if ((i == 8 && uByte >= 0x80u) || i >= MAX_BYTES) {
+                val uByte = encoded[i++].toUByte()
+                if ((i == 9 && uByte >= 0x80u) || i > MAX_BYTES) {
                     // this is the 9th and last byte we're willing to read, but it
                     // signals there's more (1 in MSB).
                     // or this is the >= 10th byte, and for some reason we're still here.
                     throw NumberFormatException("varints larger than uint63 not supported")
                 }
                 if (uByte < 0x80u) {
-                    if (uByte == 0u.toUByte() && s > 0) {
+                    if (uByte == 0u.toUByte() && i > 0) {
                         throw NumberFormatException("varint not minimally encoded")
                     }
-                    return value or (uByte.toULong() shl s)
+                    return Pair(value or (uByte.toULong() shl (i*7)), i)
                 }
-                value = value or ((uByte and 0x7fu).toULong() shl s)
-                s += 7
-                i++
+                value = value or ((uByte and 0x7fu).toULong() shl (i*7))
             }
         }
     }
