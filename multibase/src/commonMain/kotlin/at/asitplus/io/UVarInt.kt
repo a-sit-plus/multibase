89312785165f9df7607f2f702543c14097a567f3
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-07-30 13:29:21 +0200

    add start index parameter + extension functions

--- multibase/src/commonMain/kotlin/at/asitplus/io/UVarInt.kt
+++ multibase/src/commonMain/kotlin/at/asitplus/io/UVarInt.kt
@@ -27,14 +27,14 @@ data class UVarInt(private val number: ULong) {
         var acc = number
         var i = 0
         // we never encode anything larger than 2^63-1 (which fits in 9*7 bits)
-        var res = ByteArray(9)
+        var res = ByteArray(MAX_BYTES)
         while (acc >= 0x80u) {
             res[i++] = (((acc and 0x7Fu) or 0x80u).toByte())
             acc = (acc.toLong() ushr 7).toULong()
-            require(i < res.size)
+            require(i < MAX_BYTES)
         }
         res[i++] = (acc and 0x7Fu).toByte()
-        if (i == res.size) return res
+        if (i == MAX_BYTES) return res
         else return res.copyOf(i)
     }
 
@@ -43,7 +43,7 @@ data class UVarInt(private val number: ULong) {
          * Maximum number of bytes representing a UVarInt in this encoding,
          * supporting values up to 2^63 - 1.
          */
-        const val MAX_BYTES = 9L
+        const val MAX_BYTES = 9
 
         /**
          * Decodes a varint-encoded ByteArray into a UVarInt.
@@ -51,37 +51,48 @@ data class UVarInt(private val number: ULong) {
          */
         @Throws(NumberFormatException::class)
         @Deprecated(message="Specify explicitly whether you want to consume the entire byte array" +
-                "(use fromByteArrayStrict or fromByteArrayPermissive)")
+                "(use decodeFromByteArray or fromByteArrayPermissive)")
         fun fromByteArray(bytes: ByteArray): UVarInt = fromByteArrayPermissive(bytes).first
 
         /**
-         * Decodes the entirety of [bytes] into an [UVarInt].
+         * Decodes the entirety of [bytes], starting at index [startIndex], into an [UVarInt].
          * @throws NumberFormatException on illegal input (e.g. values larger than 2^63 - 1 or non-minimal encodings)
-         * @throws IllegalArgumentException if [bytes] contains trailing bytes after the [UVarInt]
+         * @throws NumberFormatException if [bytes] contains trailing bytes after the [UVarInt]
+         * @see fromByteArrayPermissive
          */
-        fun fromByteArrayStrict(bytes: ByteArray): UVarInt =
-            decode(bytes).let {
-                require(it.second == bytes.size) {
-                    "UVarInt decoding only consumed ${it.second} bytes, but ${bytes.size} were provided."
+        @Throws(NumberFormatException::class)
+        fun fromByteArrayStrict(bytes: ByteArray, startIndex: Int = 0): UVarInt =
+            decode(bytes, startIndex).let {
+                if(it.second != bytes.size) {
+                    throw NumberFormatException(
+                        "UVarInt decoding only consumed ${it.second} bytes, but ${bytes.size} were provided.")
                 }
                 UVarInt(it.first)
             }
 
         /**
-         * Decodes the leading UVarInt from [bytes].
-         * Returns the UVarInt and the number of leading bytes consumed.
+         * Decodes the leading UVarInt from [bytes], starting at index [startIndex].
+         * Returns the UVarInt, and the first index in [bytes] past the UVarInt.
          * @throws NumberFormatException on illegal input (e.g. values larger than 2^63 - 1 or non-minimal encodings)
+         * @see fromByteArrayStrict
          */
-        fun fromByteArrayPermissive(bytes: ByteArray): Pair<UVarInt,Int> =
-            decode(bytes).let { Pair(UVarInt(it.first),it.second) }
+        @Throws(NumberFormatException::class)
+        fun fromByteArrayPermissive(bytes: ByteArray, startIndex: Int = 0): Pair<UVarInt,Int> =
+            decode(bytes, startIndex).let { Pair(UVarInt(it.first),it.second) }
 
         /** Decodes a varint-encoded leading ByteArray */
-        private fun decode(encoded: ByteArray): Pair<ULong,Int> {
+        private fun decode(encoded: ByteArray, startIndex: Int): Pair<ULong,Int> {
+            require(startIndex < (Int.MAX_VALUE - MAX_BYTES))
             var value = 0uL
-            encoded.forEachIndexed { i, byte ->
-                val s = (i*7)
-                val uByte = byte.toUByte()
-                if ((i == 9 && uByte >= 0x80u) || i > MAX_BYTES) {
+            var i = startIndex
+            var s = 0
+            val last = startIndex + MAX_BYTES
+            while (true) {
+                if (i >= encoded.size) {
+                    throw NumberFormatException("varint is not terminated (overruns array)")
+                }
+                val uByte = encoded[i++].toUByte()
+                if ((i == last && uByte >= 0x80u) || i > last) {
                     // this is the 9th and last byte we're willing to read, but it
                     // signals there's more (1 in MSB).
                     // or this is the >= 10th byte, and for some reason we're still here.
@@ -91,11 +102,11 @@ data class UVarInt(private val number: ULong) {
                     if (uByte == 0u.toUByte() && s > 0) {
                         throw NumberFormatException("varint not minimally encoded")
                     }
-                    return Pair(value or (uByte.toULong() shl s), i+1)
+                    return Pair(value or (uByte.toULong() shl s), i)
                 }
                 value = value or ((uByte and 0x7fu).toULong() shl s)
+                s += 7
             }
-            throw NumberFormatException("varint is not terminated (overruns array)")
         }
     }
 }
@@ -105,4 +116,16 @@ data class UVarInt(private val number: ULong) {
  * @throws NumberFormatException on illegal input (e.g. values larger than 2^63 - 1 or non-minimal encodings)
  */
 @Throws(NumberFormatException::class)
+@Deprecated("Explicitly specify whether you want a leading UVarInt or the full byte array as UVarInt",
+    ReplaceWith("decodeAsUVarInt()"))
 fun ByteArray.varIntDecode() = UVarInt.fromByteArray(this)
+
+@Throws(NumberFormatException::class)
+/** @see UVarInt.fromByteArrayStrict */
+fun ByteArray.decodeAsUVarInt(startIndex: Int = 0) =
+    UVarInt.fromByteArrayStrict(this, startIndex)
+
+@Throws(NumberFormatException::class)
+/** @see UVarInt.fromByteArrayPermissive */
+fun ByteArray.decodeLeadingUVarInt(startIndex: Int = 0) =
+    UVarInt.fromByteArrayPermissive(this, startIndex)
--- multibase/src/commonTest/kotlin/UVarintTest.kt
+++ multibase/src/commonTest/kotlin/UVarintTest.kt
@@ -1,4 +1,5 @@
 import at.asitplus.io.UVarInt
+import at.asitplus.testballoon.matrix.CompactReport
 import at.asitplus.testballoon.matrix.matrixSuite
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
@@ -13,21 +14,35 @@ val UVarintTest by matrixSuite {
         }
     }
 
-    data("edge cases", listOf(UInt.MAX_VALUE, UInt.MAX_VALUE, 1u, 0u)) test { value ->
-        UVarInt(value).toULong().toUInt() shouldBe value
-        UVarInt.fromByteArrayStrict(UVarInt(value).encodeToByteArray()).toULong().toUInt() shouldBe value
+    data("edge cases", listOf(Long.MAX_VALUE.toULong(), UInt.MAX_VALUE.toULong(), 1uL, 0uL)) test { value ->
+        UVarInt(value).toULong() shouldBe value
+        val encoded = UVarInt(value).encodeToByteArray()
+        UVarInt.fromByteArrayStrict(encoded).toULong() shouldBe value
+        UVarInt.fromByteArrayStrict(byteArrayOf(0x21, 0x15) + encoded, 2).toULong() shouldBe value
+        UVarInt.fromByteArrayPermissive(
+            byteArrayOf(0x42, 0x53) + encoded + byteArrayOf(0x21, 0x15),
+            2
+        ).let { (decoded, i) ->
+            decoded.toULong() shouldBe value
+            i shouldBe encoded.size+2
+        }
     }
 
     "errors" {
         val AB = 0xAB.toByte()
+        val FF = 0xFF.toByte()
         // not minimally encoded zero
         shouldThrow<NumberFormatException> { UVarInt.fromByteArrayPermissive(byteArrayOf(AB, 0x0)) }
         // trailing bytes
-        shouldThrow<IllegalArgumentException> { UVarInt.fromByteArrayStrict(byteArrayOf(0x0, 0x0)) }
+        shouldThrow<NumberFormatException> { UVarInt.fromByteArrayStrict(byteArrayOf(0x0, 0x0)) }
         // runs off the end
-        shouldThrow<IllegalArgumentException> { UVarInt.fromByteArrayPermissive(byteArrayOf(AB)) }
+        shouldThrow<NumberFormatException> { UVarInt.fromByteArrayPermissive(byteArrayOf(AB)) }
         // larger than 2^63-1
         shouldThrow<IllegalArgumentException> { UVarInt(ULong.MAX_VALUE) }
-        shouldThrow<IllegalArgumentException> { UVarInt.fromByteArrayPermissive(byteArrayOf(AB, AB, AB, AB, AB, AB, AB, AB, AB, 0x01)) }
+        shouldThrow<NumberFormatException> { UVarInt.fromByteArrayPermissive(byteArrayOf(AB, AB, AB, AB, AB, AB, AB, AB, AB, 0x01)) }
+        // 2^63-1 with leading byte
+        UVarInt.fromByteArrayStrict(byteArrayOf(0x42, FF, FF, FF, FF, FF, FF, FF, FF, 0x7f), 1) shouldBe UVarInt(0x7fffffffffffffffuL)
+        // 2^64-1 with leading byte
+        shouldThrow<NumberFormatException> { UVarInt.fromByteArrayStrict(byteArrayOf(0x42, FF, FF, FF, FF, FF, FF, FF, FF, FF), 1) }
     }
 }
